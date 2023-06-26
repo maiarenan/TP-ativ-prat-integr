1ee2ab0d7d57d2a7b95407409e9926da23be8301
Renan Maia <renanmaiabh@gmail.com>
2023-06-25 22:25:01 -0300

    método post resposta questão

--- performance-review/node-api-postgres/queries.js
+++ performance-review/node-api-postgres/queries.js
@@ -36,6 +36,19 @@ const getEnunciadoQuestao = (request, response) => {
     })
 }
 
+
+const setRespostaQuestao = (request, response) => {
+    const {conceito_id, avaliacao_id, questao_id, observacao} = request.body
+    pool.query('INSERT INTO resposta (conceito_id, avaliacao_id, questao_id, observacao) VALUES ($1, $2, $3, $4)',
+             [conceito_id, avaliacao_id, questao_id, observacao], 
+             (error, results) => {
+        if(error) {
+            throw error
+        }
+        response.status(201).send(`Resposta added with ID: ${results.rows[0].id}`)
+    })
+}
+
 // const CreateUser = (request, response) => {
 //     const { name, email } = request.body
 //     pool.query('INSERT INTO usuario (name, email) VALUES ($1, $2)', [name, email], (error, results) => {
@@ -49,5 +62,6 @@ const getEnunciadoQuestao = (request, response) => {
 module.exports = {
     getUsers,
     getUserById,
-    getEnunciadoQuestao
+    getEnunciadoQuestao,
+    setRespostaQuestao
 }
